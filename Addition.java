dd18368f1eec52fe855015790b6c87e472e21dfd
MONISHA-vit <monishadevaraj238@gmail.com>
2026-03-05 18:48:02 +0530

    Implement Addition class to sum two integers

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
